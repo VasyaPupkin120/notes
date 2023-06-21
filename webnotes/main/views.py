b49333c2241e1ecdd52cfa50a2e18744ba019dd5
VasyaPupkin120 <VPupkinza@yandex.ru>
2023-06-21 23:30:24 +0700

    вывод заметок и тегов на главную

--- webnotes/main/views.py
+++ webnotes/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import Tag, Note
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    notes_list = Note.objects.all()
+    tags_list = Tag.objects.all()
+    context = {"notes_list": notes_list, "tags_list": tags_list}
+    return render(request, 'index.html', context=context)
