397b62dcd44c371ee3aa45f89cdd08fa8d87725a
VasyaPupkin120 <VPupkinza@yandex.ru>
2023-08-09 17:37:04 +0700

    добавил шаблон для строки пагинации, вставил пагинацию для главной, заметок по тегу и поиска

--- webnotes/main/views.py
+++ webnotes/main/views.py
@@ -16,6 +16,8 @@ from .forms import NoteAddForm, TagAddForm
 #     context = {"notes_list": notes_list, "tags_cloud": tags_cloud}
 #     return render(request, 'main/index.html', context=context)
 
+search_request_global = ""
+
 
 class Index(ListView):
     template_name = "main/index.html"
@@ -92,6 +94,7 @@ class TagList(ListView):
 class TagLinkPosts(ListView):
     template_name = "main/tag_link_posts.html"
     context_object_name = "notes_list"
+    paginate_by = 2
 
     def get_queryset(self):
         tag = Tag.objects.get(slug__iexact=self.kwargs["slug"])
@@ -131,7 +134,7 @@ class SearchList(ListView):
     template_name = "main/search_list.html"
     model = Note
     context_object_name = "notes_list"
-    # пока без пагинации
+    paginate_by: int = 2
 
     # для отладки 
     # def get(self, *args, **kwargs):
@@ -145,6 +148,13 @@ class SearchList(ListView):
         return notes_list
 
     def get_context_data(self, **kwargs):
+        global search_request_global
         context = super().get_context_data(**kwargs)
-        context["search_request"] = self.request.GET.get("field_search", "")
+
+        # это для сохранения значения из поля поиска - т.е. после первого перехода по пагинациям оно теряется
+        # этот код сохраняет значение и использует до получения нового значения
+        if self.request.GET.get("field_search", None):
+            search_request_global = self.request.GET["field_search"]
+        context["search_request"] = search_request_global
+
         return context
