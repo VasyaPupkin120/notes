a3a05d1578e2c93aefaad29b8291fbbe9dc690b2
VasyaPupkin120 <VPupkinza@yandex.ru>
2023-08-16 20:00:31 +0700

    перенаправление всех неаутентифицированных - на страничку аутентификации

--- webnotes/main/middlewares.py
+++ webnotes/main/middlewares.py
@@ -1,8 +1,38 @@
+from django.shortcuts import redirect
+from django.urls import reverse
 from .models import Tag
 
+
 def context_processor_tags(request):
+    """
+    Отправляет в каждый шаблон список всех тегов текущего пользователя 
+    для вставки в облако тегов.
+    """
     if request.user.is_authenticated:
         return {"tags_cloud": Tag.objects.filter(author=request.user.pk)}
     else:
         return {"tags_cloud": None}
 
+
+def CheckAuthenticationMiddleware(next_mw):
+    """
+    Перенаправляет запросы от всех неаутентифицированных пользователей
+    на страничку логина. Перенаправляет аутентифицированных пользователей
+    вместо странички логина на главную страничку.
+    """
+    def core_mw(request):
+        if request.user.is_authenticated:
+            if request.path == "/login/":
+                return redirect(reverse("index"))
+            else:
+                # переход к запрошенной вьюхе или следующему middleware
+                response = next_mw(request)
+                return response
+        else:
+            if request.path == "/login/":
+                # аналогично, просто выполняется вьюха аутентификации
+                response = next_mw(request)
+                return response
+            else:
+                return redirect(reverse("login"))
+    return core_mw
