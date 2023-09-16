5a7c544145e6cd80e5e42593cd1a42a86b56223d
VasyaPupkin120 <VPupkinza@yandex.ru>
2023-09-17 03:46:37 +0700

    красивый полный вывод записей моделей в админке

--- webnotes/main/admin.py
+++ webnotes/main/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-
 from .models import Tag, Note
 
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "author", "slug",)
+    list_display_links = ("name",)
+    search_fields = ("name", "slug")
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "slug", "created_at", "tags", )
+    list_display_links = ("title", )
+    search_fields = ("title", "slug", "created_at", "tags",)
+
+
+
 # Register your models here.
-admin.site.register(Tag)
-admin.site.register(Note)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Note, NoteAdmin)
